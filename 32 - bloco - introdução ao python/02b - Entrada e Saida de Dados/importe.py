0b3f96b3ebecd5915f325998c2ee9914f60ef3c8
Flavio Andrade <foa12381@gmail.com>
2021-12-31 09:38:24 -0300

    analize dados

--- 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/importe.py
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/importe.py
@@ -11,7 +11,21 @@ def importer_pokemon(file_path):
     except json.decoder.JSONDecodeError:
       print("Erro no Arquivo")
       return "" # como tera erro no arquivo precisamos retorna algo vazio, para nao quebrar o len
+    except FileNotFoundError:
+      print("Erro no caminho do Arquivo")
+      return ""
+
+def get_pokemons(pokemon):
+  pokemons = set()
+  for p in pokemon:
+    pokemons.add(p["name"])
+  return pokemons
+
 
 if __name__ == '__main__':
   data = importer_pokemon("./pokemon.json")
   print(len(data))
+  print(data[0]["name"])
+
+  pokemons = get_pokemons(data)
+  print(pokemons)
