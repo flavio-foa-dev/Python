6f879316394c0045089bd69b13fa3c09c2f52879
Flavio Andrade <foa12381@gmail.com>
2021-12-31 08:04:30 -0300

    csv e json em python

--- 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/importe.py
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/importe.py
@@ -1,2 +1,17 @@
-def importer(file_path):
-  pass
+import json
+
+print("oi")
+
+
+
+def importer_pokemon(file_path):
+    try:
+        with open(file_path) as file_pokemon:
+            return json.load(file_pokemon)
+    except json.decoder.JSONDecodeError:
+      print("Erro no Arquivo")
+      return "" # como tera erro no arquivo precisamos retorna algo vazio, para nao quebrar o len
+
+if __name__ == '__main__':
+  data = importer_pokemon("./pokemon.json")
+  print(len(data))
