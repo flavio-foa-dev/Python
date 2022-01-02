5c9eee7f0c4dc220110d603b437a4e4c2819a7c5
Flavio Andrade <foa12381@gmail.com>
2022-01-02 09:27:34 -0300

    from location name

--- 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/analyzer.py
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/analyzer.py
@@ -1,9 +1,13 @@
 from src.importer import importeer_file
 
 def get_herois(herois):
-  pass
+  #print(herois)
 
+  hero = set()
+  for h in herois:
+    hero.add(h["publisher"])
+  return hero
 
 if __name__ == '__main__':
   herois = importeer_file("data/superheroes.json")
-  print(herois[0])
+  print(get_herois(herois))
