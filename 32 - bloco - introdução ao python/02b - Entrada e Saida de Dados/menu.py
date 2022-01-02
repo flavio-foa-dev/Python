433a16a1b1447f9c1504ec5d36ffb96eb97d8d37
Flavio Andrade <foa12381@gmail.com>
2022-01-02 13:28:58 -0300

    task de menu

--- 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/menu.py
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/menu.py
@@ -1,7 +1,19 @@
+import csv
 from analyzer import sales_mivies
 from src.importer import importeer_file
 print("menu")
 
+
+
+def export(data):
+  try:
+    with open("data/exporte.csv", mode="w") as file:
+      report_write = csv.writer(file)
+      for key, value in data.items():
+        report_write.writerow([key, value])
+  except FileNotFoundError:
+    print("arquivo nao encontrado")
+    return ""
 
 START_MENU = """
 1- vendas por Empresa
@@ -11,10 +23,11 @@ START_MENU = """
 
 if __name__ == '__main__':
     herois = importeer_file("data/superheroes.json")
-    print(herois)
     value =  input(f"escola uma opção{START_MENU}")
     if value == "1":
-      print(sales_mivies(herois))
+      data = sales_mivies(herois)
+      print(data)
+      export(data)
     elif value == "2":
       print("saiu")
     else:
