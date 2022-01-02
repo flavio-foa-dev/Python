22b6f2cb50fadfba759db8c6a12968a46516cc86
Flavio Andrade <foa12381@gmail.com>
2022-01-02 18:45:56 -0300

    csn json

--- 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/aula/aula.py
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/aula/aula.py
@@ -6,9 +6,16 @@ print("rodando...")
 
 
 with open("../data/superheroes.json", mode='r') as file:
-  print(json.load(file)[1])
-  print(file.read())
+  hero = json.load(file)
+  for h in hero:
+    print(h["superhero"])
+  # print(json.load(file)[1])
+  # print(file.read())
 
 
-# with open("../data/cisade.csv", mode='r') as file:
-#   print(file.read())
+with open("../data/cisade.csv", mode='r') as file:
+    local = csv.DictReader(file)
+    num = 1
+    for l in local:
+      print(num, l['ponto_codigo'])
+      num =+ 1 # nao consigo fazer ficar dinamico
