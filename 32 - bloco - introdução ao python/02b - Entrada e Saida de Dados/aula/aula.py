d527bc20b41a5576f203532dc9869ef285b6e4ff
Flavio Andrade <foa12381@gmail.com>
2022-01-02 19:06:52 -0300

    csv

--- 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/aula/aula.py
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/aula/aula.py
@@ -14,8 +14,10 @@ with open("../data/superheroes.json", mode='r') as file:
 
 
 with open("../data/cisade.csv", mode='r') as file:
-    local = csv.DictReader(file)
+    local, * res = csv.DictReader(file)
+    print(local)
+    # print(res)
     num = 1
-    for l in local:
+    for l in res:
       print(num, l['ponto_codigo'])
       num =+ 1 # nao consigo fazer ficar dinamico
