b0b8390e2587a42c1cbd3734dc5d2724184f26fe
Flavio Andrade <foa12381@gmail.com>
2022-01-02 12:59:00 -0300

    task de menu

--- 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/menu.py
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/menu.py
@@ -3,7 +3,19 @@ from src.importer import importeer_file
 print("menu")
 
 
+START_MENU = """
+1- vendas por Empresa
+2- sair
+"""
 
 
 if __name__ == '__main__':
-  pass
+    herois = importeer_file("data/superheroes.json")
+    print(herois)
+    value =  input(f"escola uma opção{START_MENU}")
+    if value == "1":
+      print(sales_mivies(herois))
+    elif value == "2":
+      print("saiu")
+    else:
+      print("opção invalida")
