cb92d8bd2fe53d4666ae8f65679ab6092c640ca6
Flavio Andrade <foa12381@gmail.com>
2022-01-02 10:40:23 -0300

    menu python

--- 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/analyzer.py
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/analyzer.py
@@ -8,6 +8,16 @@ def get_herois(herois):
     hero.add(h["publisher"])
   return hero
 
+def sales_mivies(herois):
+  result_sale = { price:0 for price in get_herois(herois) }
+
+  for h in herois:
+      hero = h["publisher"]
+      result_sale[hero]+=int(h["price"]["valor"])
+
+  return result_sale
+
 if __name__ == '__main__':
   herois = importeer_file("data/superheroes.json")
   print(get_herois(herois))
+  print(sales_mivies(herois))
---
+++ 32 - bloco - introdução ao python/02b - Entrada e Saida de Dados/menu.py
@@ -0,0 +1,9 @@
+from analyzer import sales_mivies
+from src.importer import importeer_file
+print("menu")
+
+
+
+
+if __name__ == '__main__':
+  pass
