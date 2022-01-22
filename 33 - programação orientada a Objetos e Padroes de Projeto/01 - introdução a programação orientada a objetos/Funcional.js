dfb062c5465fd7472a40e35d5a438f1828780e80
Flavio Andrade <foa12381@gmail.com>
2022-01-22 15:37:49 -0300

    resolve bug no sort()

--- 33 - programação orientada a Objetos e Padroes de Projeto/01 - introdução a programação orientada a objetos/Funcional.js
+++ 33 - programação orientada a Objetos e Padroes de Projeto/01 - introdução a programação orientada a objetos/Funcional.js
@@ -9,3 +9,48 @@ console.log(numbers)
 numbers.sort((a,b) => a - b)
   .map((number)=> `Eu sou o numero ${number}`)
   .forEach((number) => {console.log(number)})
+
+console.log("______________-________por letra _____________________________")
+
+  var items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic' },
+    { name: 'Zeros', value: 37 }
+  ];
+  items.sort(function (a, b) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+
+  console.log(items)
+
+ console.log("_______________________________por numero _____________________________________________")
+  var itens = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic' },
+    { name: 'Zeros', value: 37 }
+  ];
+  itens.sort(function (a, b) {
+    if (a.value > b.value) {
+      return 1;
+    }
+    if (a.value < b.value) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+
+  console.log(itens)
