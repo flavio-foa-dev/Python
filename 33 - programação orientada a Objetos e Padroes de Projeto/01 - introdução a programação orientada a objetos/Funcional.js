00e49b9c0a956f06e6ff8117e824b990032095b8
Flavio Andrade <foa12381@gmail.com>
2022-01-22 15:31:27 -0300

    resolve bug no sort()

--- 33 - programação orientada a Objetos e Padroes de Projeto/01 - introdução a programação orientada a objetos/Funcional.js
+++ 33 - programação orientada a Objetos e Padroes de Projeto/01 - introdução a programação orientada a objetos/Funcional.js
@@ -1,5 +1,11 @@
 const numbers = [1, 45, 67, 75 , 5, 0, 40]
+numbers.sort(function(a,b){
+  return a -b
+})
 
-numbers.sort()
+console.log(numbers)
+
+
+numbers.sort((a,b) => a - b)
   .map((number)=> `Eu sou o numero ${number}`)
   .forEach((number) => {console.log(number)})
