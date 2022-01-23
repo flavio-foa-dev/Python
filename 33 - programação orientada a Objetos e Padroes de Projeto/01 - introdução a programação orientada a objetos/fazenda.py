74936c157f3f13a38fd16b19927f0a8947e2ee39
Flavio Andrade <foa12381@gmail.com>
2022-01-23 09:28:12 -0300

    interaçoes entre classe class

--- 33 - programação orientada a Objetos e Padroes de Projeto/01 - introdução a programação orientada a objetos/fazenda.py
+++ 33 - programação orientada a Objetos e Padroes de Projeto/01 - introdução a programação orientada a objetos/fazenda.py
@@ -4,7 +4,33 @@ class Cachorro:
     tipo_animal = "mamifero"
 
 
+    def __init__(self, name):
+        self.name = name
+
+    def grita(self):
+        print(f"{self.name}: auu auuu...")
+
+
+
+class Galinha:
+    cobertura = 'pena'
+    patas = 2
+    tipo_animal = 'ave'
+
+    def __init__(self, name):
+        self.name = name
+
+    def grita(self):
+        print(f"{self.name}: cocorico...")
 
 if __name__ == '__main__':
-    rex = Cachorro()
+    rex = Cachorro("Rex")
+    lili = Galinha("Lili")
+
     print(rex.cobertura)
+    rex.grita()
+
+    print (lili.cobertura)
+    lili.grita()
+
+    print(f"{rex.grita()}: {lili.grita()} Socorro")
